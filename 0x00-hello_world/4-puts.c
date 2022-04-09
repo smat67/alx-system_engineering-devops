a43b18ca3ef763950caca06a5ae1726e733be2fa
smat67 <sheillahm@gmail.com>
2022-04-09 16:31:02 -0700

    task 4-6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
-*main - entry point 
-*
-*Return: 0 after successful
-*/
+ *main - entry point 
+ *
+ *Return: 0 after successful
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle"); 
+	return (0); 
 }
 
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *my hello. printf task
+ *Return: 1 after program executed
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
