73845a5f78e73952eeec3d456b6997ef9f62d3f7
smat67 <sheillahm@gmail.com>
2022-04-10 09:32:04 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main -Entry menu
+ *
+ * Return: 0 always if successful
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alphabet == 'q')
+			continue;
+		else if (alphabet == 'e')
+		continue;
+
+		putchar(alphabet);
+	}
+	
+	putchar('\n');
+	return (0);
+}
