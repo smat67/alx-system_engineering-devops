af4065387a3750728abab8dc793acc39c5d3e12e
smat67 <sheillahm@gmail.com>
2022-04-11 06:00:43 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: 1 always if successful
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 20
+
+			15-10-19\n", 59);
+	return(1);
+}
+
