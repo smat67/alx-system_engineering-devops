9a9be81225c96f8b71bf6963f83df072f95edc90
smat67 <sheillahm@gmail.com>
2022-04-11 16:02:11 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,11 +15,10 @@ int main(void)
 	{
 		putchar((digit1 % 10) + '0');
 		putchar((digit2 % 10) + '0');
-	 	if (digit1 == 8 && digit2 == 9)
+		if (digit1 == 8 && digit2 == 9)
 			continue;
-
-	putchar(',');
-	putchar(' ');
+		putchar(',');
+		putchar(' ');
 	}
 	}
 	putchar('\n');
