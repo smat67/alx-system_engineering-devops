64c0d5a56c4f4beaa7e89dbf0bc0898ced8d7503
smat67 <sheillahm@gmail.com>
2022-04-13 16:18:13 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+nclude "main.h"
+/**
+ * prints_alphabet_x10 - Prints in lowercase
+ */
+void prints_alphabet_x10(void)
+{
+	int count = 0;
+	char letter;
+
+	while (count++ <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
+}
+
+
