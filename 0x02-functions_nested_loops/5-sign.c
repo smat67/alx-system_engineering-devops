63b313fe505f1418f63d7e4276f6265c94e72069
smat67 <sheillahm@gmail.com>
2022-04-13 17:23:44 -0700

    task6

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_sign : prints the sign of a number.
+ * print_sign : prints the sign of a number .
  * @n: sign number to be printed
  *
  * Return: 1 if number is greater than zero,
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @n: The integer to be computed.
+ *
+ * Return: the absolute value of the integer.
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
