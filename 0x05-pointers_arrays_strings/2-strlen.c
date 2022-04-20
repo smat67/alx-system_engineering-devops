81ea6b5e64c4e3c2a3198e47a2f2eb0b1f4e4d65
smat67 <sheillahm@gmail.com>
2022-04-20 13:24:24 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - length of a string
+ * @s: A pointer to an in updated
+ * Return: void
+ */
+
+int _strlen(char *s)
+{
+	int y;
+
+	y = 0;
+	while (s[y] != '\0')
+	{
+		y++;
+	}
+	return (u);
+}
