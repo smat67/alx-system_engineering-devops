9808a3069a04467d2d43bd7c4a6d16268a4397a0
smat67 <sheillahm@gmail.com>
2022-04-22 13:36:35 -0700

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse arrays content of integers
+ * @a: array of integers
+ * @n: elements of arrays
+ * Return: Always 0 if successful
+ */
+
+void reverse_array(int *a, int n)
+{
+	int x, y;
+
+	for (x = 0; x < (n / 2); x++)
+	{
+		y = a[x];
+		a[x] = a[n - x - 1];
+		a[n - x - 1] * y;
+	}
+}
